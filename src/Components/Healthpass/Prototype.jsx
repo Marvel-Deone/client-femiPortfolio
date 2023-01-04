cdb62f12407c9d40537ca39b8b331b7faab54205
Marvel-Deone <89777325+Marvel-Deone@users.noreply.github.com>
2023-01-04 01:48:28 +0100

    Added some changes

---
+++ src/Components/Healthpass/Prototype.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Prototype = () => {
+    return (
+        <div className="max-w-4xl mx-auto p-4 md:px-20 mb-10 lg:mb-20">
+            <div className="flex flex-col gap-8 md:gap-0">
+                <div className="flex flex-col md:flex-row gap-4 md:gap-16">
+                    <div className="flex flex-col my-auto gap-4">
+                        <div>
+                            <h2 className="font-semibold leading-4 text-base">Try Protype Out</h2>
+                            <h2 className="text-[15px] mt-5">I created a digital experience for both indiviaduals and organizations. this mobile app enabled real time track of test results . Navigate to “Click here” to see prototype
+                            </h2>
+                        </div>
+                    </div>
+                    <iframe title="prototype" className="bg-transparent" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FcQsrCQOIi3PODesP1jeU5B%2Fhealth-pass-new%3Fpage-id%3D0%253A1%26node-id%3D1%253A433%26viewport%3D468%252C211%252C0.13%26scaling%3Dcontain%26starting-point-node-id%3D1%253A433" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Prototype
--- src/Pages/HealthPass.jsx
+++ src/Pages/HealthPass.jsx
@@ -4,6 +4,7 @@ import Definition from "../Components/Healthpass/Definition"
 import Design from "../Components/Healthpass/Design"
 import NextProject from "../Components/Healthpass/NextProject"
 import Overview from "../Components/Healthpass/Overview"
+import Prototype from "../Components/Healthpass/Prototype"
 import SolutionChallenge from "../Components/Healthpass/SolutionChallenge"
 import Takeaway from "../Components/Healthpass/Takeaway"
 import TailNav from "../Components/TailNav"
@@ -19,6 +20,7 @@ const HealthPass = () => {
             <Design />
             <SolutionChallenge />
             <Definition />
+            <Prototype />
             <Takeaway />
             <NextProject />
             <Footer />
