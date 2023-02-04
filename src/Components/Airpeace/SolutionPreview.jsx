5a0426926cdbdb6c61b983ed19cab84b5b27cd77
Marvel-Deone <89777325+Marvel-Deone@users.noreply.github.com>
2023-02-04 19:40:35 +0100

    New update

--- src/Components/Airpeace/SolutionPreview.jsx
+++ src/Components/Airpeace/SolutionPreview.jsx
@@ -7,16 +7,23 @@ const SolutionPreview = () => {
             <div className="flex flex-col gap-8 md:gap-0">
                 {/* select location */}
                 <div className="flex flex-col md:flex-row gap-3 md:gap-56">
-                    <div className="relative w-fit h-[28rem] md:h-[30rem] lg:h-[36rem] flex items-center px-2">
+                    <div className="md:hidden relative w-fit h-[28rem] md:h-[30rem] lg:h-[36rem] flex items-center px-2">
                         <div className="rounded-full bg-[#E9F1FC] w-72 h-72 md:w-80 md:h-80 lg:w-96 lg:h-96 flex my-auto">
                         </div>
                         <img src="assets/images/14.png"
                             className="absolute mx-auto top-0 h-[28rem] md:h-[30rem] lg:h-[36rem] object-cover" alt="" />
                     </div>
+
+                    <div className="hidden md:flex relative w-fit h-[28rem] md:h-[36rem]">
+                        <div className="rounded-full bg-[#E9F1FC] w-72 h-72  md:w-80 md:h-80 lg:w-96 lg:h-96 flex my-auto">
+                        </div>
+                        <img src="assets/images/14.png"
+                            className="absolute mx-auto left-[0.625rem] top-0 h-[28rem] md:h-[36rem] object-cover" alt="" />
+                    </div>
                     <div className="flex flex-col my-auto gap-3 md:gap-12">
-                        <h2 className="font-bold text-lg md:text-2xl text-[#111111]w-full md:w-[28rem]">A ready to use app, all accessible from your mobile phone.
+                        <h2 className="font-bold text-lg md:text-2xl text-[#111111]w-full md:w-[28rem] px-2">A ready to use app, all accessible from your mobile phone.
                         </h2>
-                        <p className="text-base text-[#111111] font-[300] w-full md:w-[28rem]">Whether a user wants to Book a Flight, view Flight Status, Check in or Manage Bookings. </p>
+                        <p className="text-base text-[#111111] font-[300] w-full md:w-[28rem] px-2">Whether a user wants to Book a Flight, view Flight Status, Check in or Manage Bookings. </p>
                     </div>
                 </div>
                 {/* List of departure */}
@@ -44,16 +51,22 @@ const SolutionPreview = () => {
                 </div>
                 {/* Boarding Pass generated */}
                 <div className="flex flex-col md:flex-row gap-3 md:gap-64 px-2">
-                    <div className="relative w-fit h-[28rem] md:h-[36rem] flex">
+                    <div className="md:hidden relative w-fit h-[28rem] md:h-[36rem] flex">
                         <div className="rounded-full bg-[#E9F1FC] w-72 h-72 md:w-80 md:h-80 lg:w-96 lg:h-96 flex my-auto">
                         </div>
                         <img src="assets/images/airpeace-3.png"
                             className="absolute mx-auto top-0 left-[0.6rem] h-[28rem] md:h-[36rem] object-cover" alt="" />
                     </div>
+                    <div className="hidden md:flex relative w-fit h-[28rem] md:h-[36rem]">
+                        <div className="rounded-full bg-[#E9F1FC] w-72 h-72  md:w-80 md:h-80 lg:w-96 lg:h-96 flex my-auto">
+                        </div>
+                        <img src="assets/images/airpeace-3.png"
+                            className="absolute mx-auto left-[0.625rem] top-0 h-[28rem] md:h-[36rem] object-cover" alt="" />
+                    </div>
                     <div className="flex flex-col my-auto gap-3 md:gap-12">
-                        <h2 className="font-bold text-lg md:text-2xl text-[#111111] w-full md:w-[28rem]">View Upcoming Trip.
+                        <h2 className="font-bold text-lg md:text-2xl text-[#111111] w-full md:w-[28rem] px-1">View Upcoming Trip.
                         </h2>
-                        <p className="text-base text-[#111111] font-[300] w-full md:w-[26rem]">Air Travellers can view details of their next trip. from the date to the time and also the flight number.</p>
+                        <p className="text-base text-[#111111] font-[300] w-full md:w-[26rem] px-1">Air Travellers can view details of their next trip. from the date to the time and also the flight number.</p>
                     </div>
                 </div>
             </div>
