64118a0955859f76d5ecbd025f1c0be9e0b56c77
Marvel-Deone <89777325+Marvel-Deone@users.noreply.github.com>
2023-03-04 11:19:57 +0100

    Added new update

--- src/Components/Airpeace/DesignChallenge.jsx
+++ src/Components/Airpeace/DesignChallenge.jsx
@@ -5,24 +5,24 @@ const DesignChallenge = () => {
             <div className="max-w-6xl mx-auto px-3 py-6 md:py-0 flex flex-col gap-7">
                 <h2 className="font-semibold leading-4 text-base text-[#ffffff]">Design Challenge</h2>
                 <div className="flex flex-col gap-3">
-                    <p className="text-sm md:text-[18px] leading-loose text-white font-[200]">In the past few years, the
+                    <p className="text-sm md:text-base leading-loose text-white font-[200]">In the past few years, the
                         Aviation sector
                         in Nigeria has
                         witnessed a gradual increase in domestic and international flights and the use of online booking
                         mobile apps by air travelers is also rising.
                     </p>
-                    <p className="text-white text-sm md:text-[18px] leading-loose font-[200]">
+                    <p className="text-white text-sm md:text-base leading-loose font-[200]">
                         Before booking for flights
                         via
                         mobile
                         applications or websites air
                         travelers often ended up on
                         long queues at the airport to purchase a ticket. <br /></p>
-                    <p className="text-white text-sm md:text-[18px] leading-loose font-[200] w-full md:w-[444px] border-b-2 border-blue-800">Here’s our initial “How Might We“ (HMW) Problem statement</p>
+                    <p className="text-white text-sm md:text-base leading-loose font-[200] w-full md:w-[444px] border-b-2 border-blue-800">Here’s our initial “How Might We“ (HMW) Problem statement</p>
                 </div>
             </div>
             <div className="max-w-6xl flex mx-auto px-3 py-10">
-                <h2 className="text-white text-[16px] md:text-2xl text-extrabold">HMW encourage easy and seamless purchase of air
+                <h2 className="text-white  text-[14px] md:text-2xl text-bold">HMW encourage easy and seamless purchase of air
                     tickets
                     by
                     air
